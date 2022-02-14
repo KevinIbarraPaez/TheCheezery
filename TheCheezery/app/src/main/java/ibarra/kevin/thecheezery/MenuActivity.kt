e11a39e579393ae12f53255bbffa0e7a93e54d52
KevinIbarraPaez <98064423+KevinIbarraPaez@users.noreply.github.com>
2022-02-13 21:32:18 -0700

    Add files via upload

---
+++ TheCheezery/app/src/main/java/ibarra/kevin/thecheezery/MenuActivity.kt
@@ -0,0 +1,11 @@
+package ibarra.kevin.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
